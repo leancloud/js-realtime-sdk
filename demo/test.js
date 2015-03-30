392b15e5f0c374e3b2f1975707425fcb7597a6df
尼奥 <1988wangxiao@gmail.com>
2015-03-30 16:59:55 +0800

    [feature] Demo 优化界面。

--- demo/test.js
+++ demo/test.js
@@ -1,219 +1,229 @@
 var rt;
 var conv;
-
-// 已有房间
-var conv2;
+// 请将 AppId 改为你自己的 AppId
+var appId = '9p6hyhh60av3ukkni3i9z53q1l8yy3cijj6sie3cewft18vm';
 var firstFlag = true;
 
-// 创建聊天实例（支持单页多实例）
-rt = AV.realtime({
-    // 强将 appId 换为自己的 appId
-    appId: '9p6hyhh60av3ukkni3i9z53q1l8yy3cijj6sie3cewft18vm',
-    // appId: 'pyon3kvufmleg773ahop2i7zy0tz2rfjx5bh82n7h5jzuwjg',
-    clientId: 'LeanCloud111'
-    // 是否开启服务器端认证
-    // auth: authFun
-});
+// 已有 converstion
+var conv2;
 
-// 当前 SDK 版本
-console.log('欢迎使用 LeanCloud 实时通信，当前 SDK 版本是 ' + AV.realtime.version);
+// Demo 中聊天相关的主逻辑
+function main() {
 
-// 实时通信服务连接成功
-rt.on('open', function() {
-    console.log('实时通信服务建立成功！');
+    // 创建聊天实例（支持单页多实例）
+    rt = AV.realtime({
+        appId: appId,
+        // appId: 'pyon3kvufmleg773ahop2i7zy0tz2rfjx5bh82n7h5jzuwjg',
+        clientId: 'LeanCloud111'
+        // 是否开启服务器端认证
+        // auth: authFun
+    });
 
-    // 因为断开重连还会触发一次 open 事件，所以用一个状态标记下
     if (firstFlag) {
-        firstFlag = false;
-
-        // 创建一个聊天室
-        conv = rt.conv({
-            // 人员的 id
-            members: [
-                'LeanCloud02'
-            ],
-            // 默认的数据，可以放 Conversation 名字等
-            data: {
-                m: 123
-            }
-        }, function(data) {
-            if (data) {
-                console.log('Conversation 创建成功!', data);
-            }
-        });
-
-        // 查询当前 Conversation 的相关信息
-        rt.query(function(data) {
-            console.log('查询 Conversation 所有相关信息：', data);
-        });
-
-        // 查询对应 clientId 的用户是否处于在线状态
-        rt.ping([
-            'LeanCloud111',
-            'LeanCloud02'
-        ], function(data) {
-            console.log('查询用户在线状态：', data);
-        });
+        // 当前 SDK 版本
+        showLog('欢迎使用 LeanCloud 实时通信，当前 SDK 版本是 ' + AV.realtime.version);
     }
-});
 
-// 当聊天断开时触发
-rt.on('close', function() {
-    console.log('实时通信服务被断开！');
-});
+    // 实时通信服务连接成功
+    rt.on('open', function() {
+        showLog('实时通信服务建立成功！');
 
-// 当 Conversation 被创建时触发，当然您可以使用回调函数来处理，不一定要监听这个事件
-rt.on('create', function(data) {
-    // 当前用户加入这个 Conversation 
-    conv.join(function(data) {
-        console.log('当前用户成功加入 Conversation');
-    });
-    // 向这个 Conversation 添加新的用户
-    conv.add([
-        'LeanCloud03', 'LeanCloud04'
-    ], function(data) {
-        console.log('成功添加用户：', data);
-    });
+        // 因为断开重连还会触发一次 open 事件，所以用一个状态标记下
+        if (firstFlag) {
+            firstFlag = false;
 
-    // 从这个 Conversation 中删除用户
-    conv.remove('LeanCloud03', function(data) {
-        console.log('成功删除用户：', data);
-    });
+            // 创建一个聊天室
+            conv = rt.conv({
+                // 人员的 id
+                members: [
+                    'LeanCloud02'
+                ],
+                // 默认的数据，可以放 Conversation 名字等
+                data: {
+                    m: 123
+                }
+            }, function(data) {
+                if (data) {
+                    console.log('Conversation 创建成功!', data);
+                }
+            });
 
-    // 当前用户离开这个 Conversation 
-    conv.leave(function(data) {
-        console.log('当前用户成功离开 Conversation');
-    });
+            // 查询当前 Conversation 的相关信息
+            rt.query(function(data) {
+                console.log('查询 Conversation 所有相关信息：', data);
+            });
 
-    // 向这个 Conversation 中发送消息
-    conv.send({
-        abc: 123
-    }, function(data) {
-        console.log('发送的消息服务端已收收到：', data);
-
-        // 查看历史消息
-        conv.log(function(data) {
-            console.log('查看当前 Conversation 最近的聊天记录：', data);
-        });
-    });
-
-    // 向这个 Conversation 中发送暂态消息
-    conv.send({
-        msg: '当前用户正在输入。。。'
-    }, {
-        // 暂态消息，不需要回调
-        transient: true
-    }, function(data) {
-        console.log('暂态消息的回调不会被运行');
-    });
-
-    // 向这个 Conversation 中发送消息，并且消息是否对方收到要有回执
-    conv.send({
-        abc: 123
-    }, {
-        // 获取阅读回执
-        receipt: true
-    }, function(data) {
-        console.log('信息发送成功，该信息会获取阅读回执');
-    });
-
-    // 当前 Conversation 接收到消息
-    conv.receive(function(data) {
-        console.log('当前 Conversation 收到消息：', data);
-    });
-
-    // 当前 Conversation 接收到消息
-    conv.receipt(function(data) {
-        console.log('当前 Conversation 收到消息回执：', data);
-    });
-
-    // 获取当前 Conversation 中的成员信息
-    conv.list(function(data) {
-        console.log('列出当前 Conversation 的成员列表：', data);
-    });
-
-    // 发送多媒体消息
-    conv.send({
-        text: '图片测试',
-        // 自定义的属性
-        attr: {
-            a:123
-        },
-        url: 'https://leancloud.cn/images/static/press/Logo%20-%20Blue%20Padding.png',
-        metaData: {
-            name:'logo',
-            format:'png',
-            height: 123,
-            width: 123,
-            size: 888
+            // 查询对应 clientId 的用户是否处于在线状态
+            rt.ping([
+                'LeanCloud111',
+                'LeanCloud02'
+            ], function(data) {
+                console.log('查询用户在线状态：', data);
+            });
         }
-    }, {
-       type: 'image'
-    }, function(data) {
-        console.log('图片数据发送成功！');
     });
 
-    // 取得当前 conv 中的人数
-    conv.count(function(num) {
-        console.log('取得当前的用户数量：' + num);
+    // 当聊天断开时触发
+    rt.on('close', function() {
+        console.log('实时通信服务被断开！');
     });
 
-    // 这是一个已有的对话，通过房间 id 生成它的对话实例
-    conv2 = rt.conv('55150e62e4b0d4d151ef12cf', function() {
-        console.log('已经获取已有房间的实例');
+    // 当 Conversation 被创建时触发，当然您可以使用回调函数来处理，不一定要监听这个事件
+    rt.on('create', function(data) {
+        // 当前用户加入这个 Conversation 
+        conv.join(function(data) {
+            console.log('当前用户成功加入 Conversation');
+        });
+        // 向这个 Conversation 添加新的用户
+        conv.add([
+            'LeanCloud03', 'LeanCloud04'
+        ], function(data) {
+            console.log('成功添加用户：', data);
+        });
+
+        // 从这个 Conversation 中删除用户
+        conv.remove('LeanCloud03', function(data) {
+            console.log('成功删除用户：', data);
+        });
+
+        // 当前用户离开这个 Conversation 
+        conv.leave(function(data) {
+            console.log('当前用户成功离开 Conversation');
+        });
+
+        // 向这个 Conversation 中发送消息
+        conv.send({
+            abc: 123
+        }, function(data) {
+            console.log('发送的消息服务端已收收到：', data);
+
+            // 查看历史消息
+            conv.log(function(data) {
+                console.log('查看当前 Conversation 最近的聊天记录：', data);
+            });
+        });
+
+        // 向这个 Conversation 中发送暂态消息
+        conv.send({
+            msg: '当前用户正在输入。。。'
+        }, {
+            // 暂态消息，不需要回调
+            transient: true
+        }, function(data) {
+            console.log('暂态消息的回调不会被运行');
+        });
+
+        // 向这个 Conversation 中发送消息，并且消息是否对方收到要有回执
+        conv.send({
+            abc: 123
+        }, {
+            // 获取阅读回执
+            receipt: true
+        }, function(data) {
+            console.log('信息发送成功，该信息会获取阅读回执');
+        });
+
+        // 当前 Conversation 接收到消息
+        conv.receive(function(data) {
+            console.log('当前 Conversation 收到消息：', data);
+        });
+
+        // 当前 Conversation 接收到消息
+        conv.receipt(function(data) {
+            console.log('当前 Conversation 收到消息回执：', data);
+        });
+
+        // 获取当前 Conversation 中的成员信息
+        conv.list(function(data) {
+            console.log('列出当前 Conversation 的成员列表：', data);
+        });
+
+        // 发送多媒体消息
+        conv.send({
+            text: '图片测试',
+            // 自定义的属性
+            attr: {
+                a:123
+            },
+            url: 'https://leancloud.cn/images/static/press/Logo%20-%20Blue%20Padding.png',
+            metaData: {
+                name:'logo',
+                format:'png',
+                height: 123,
+                width: 123,
+                size: 888
+            }
+        }, {
+           type: 'image'
+        }, function(data) {
+            console.log('图片数据发送成功！');
+        });
+
+        // 取得当前 conv 中的人数
+        conv.count(function(num) {
+            console.log('取得当前的用户数量：' + num);
+        });
+
+        // 这是一个已有的对话，通过房间 id 生成它的对话实例
+        conv2 = rt.conv('55150e62e4b0d4d151ef12cf', function() {
+            console.log('已经获取已有房间的实例');
+        });
+
+        conv2.add([
+            'LeanCloud05', 'LeanCloud06'
+        ], function(data) {
+            console.log('已有的房间成功添加新的用户：', data);
+        });
+
+        bindReceive();
     });
 
-    conv2.add([
-        'LeanCloud05', 'LeanCloud06'
-    ], function(data) {
-        console.log('已有的房间成功添加新的用户：', data);
-    });    
-});
+    // 监听所有用户加入的情况
+    rt.on('join', function(data) {
+        console.log('有用户加入某个当前用户在的 Conversation：', data);
+    });
 
-// 监听所有用户加入的情况
-rt.on('join', function(data) {
-    console.log('有用户加入某个当前用户在的 Conversation：', data);
-});
+    // 监听所有用户离开的情况
+    rt.on('left', function(data) {
+        console.log('有用户离开某个当前用户在的 Conversation：', data);
+    });
 
-// 监听所有用户离开的情况
-rt.on('left', function(data) {
-    console.log('有用户离开某个当前用户在的 Conversation：', data);
-});
+    // 监听所有 Conversation 中发送的消息
+    rt.on('message', function(data) {
+        console.log('某个当前用户在的 Conversation 接收到消息：', data);
+    });
 
-// 监听所有 Conversation 中发送的消息
-rt.on('message', function(data) {
-    console.log('某个当前用户在的 Conversation 接收到消息：', data);
-});
+    // 监听短信回执事件
+    rt.on('receipt', function(data) {
+        console.log('接收到消息阅读的回执：', data);
+    });
 
-// 监听短信回执事件
-rt.on('receipt', function(data) {
-    console.log('接收到消息阅读的回执：', data);
-});
+    // 接收断线或者网络状况不佳的事件（断网可测试）
+    rt.on('reuse', function() {
+        console.log('正在重新连接。。。');
+    });
 
-// 接收断线或者网络状况不佳的事件（断网可测试）
-rt.on('reuse', function() {
-    console.log('正在重新连接。。。');
-});
+    // 当然你可以关闭这一切
+    // setTimeout(function() {
+    //     rt.close();
+    // }, 10000);
 
-// 当然你可以关闭这一切
-// setTimeout(function() {
-//     rt.close();
-// }, 10000);
+    var eventFun = function(data) {
+        console.log('接收到自定义事件', data);
+    };
 
-var eventFun = function(data) {
-    console.log('接收到自定义事件', data);
-};
+    // 监听自定义事件
+    rt.on('LeanCloud123', eventFun);
 
-// 监听自定义事件
-rt.on('LeanCloud123', eventFun);
+    // 取消绑定自定义事件
+    rt.off('LeanCloud123', eventFun);
 
-// 取消绑定自定义事件
-rt.off('LeanCloud123', eventFun);
+    // 派发自定义事件
+    rt.emit('LeanCloud123', {
+        test: 123
+    });
 
-// 派发自定义事件
-rt.emit('LeanCloud123', {
-    test: 123
-});
+}
 
 function authFun(options, callback) {
     // 将以上签名必要参数及当前应用状态信息发送到应用服务器端做权限判
@@ -232,4 +242,69 @@ function authFun(options, callback) {
         },
         method: 'post'
     }, callback);
+}
+
+// demo 中输出代码
+function showLog(msg, data) {
+    if (data) {
+        console.log(msg, data);
+        msg += JSON.stringify(data);
+    } else {
+        console.log(msg);
+    }
+    var div = document.getElementById('print-wall');
+    var p = document.createElement('p');
+    p.innerText = msg;
+    div.appendChild(p);
+}
+
+var openBtn = document.getElementById('open-btn');
+var sendBtn = document.getElementById('send-btn');
+
+openBtn.addEventListener('click', function() {
+    var val = document.getElementById('input-appId').value;
+    if (val) {
+        appId = val;
+    }
+    main();
+});
+
+sendBtn.addEventListener('click', function() {
+    if (firstFlag) {
+        alert('请先连接服务器！');
+        return;
+    }
+    var val = document.getElementById('input-send').value;
+    conv2.send({
+        test: val
+    }, function(data) {
+        showLog('发送的消息服务端已收收到，查看另一个客户端，可能已经收到这条消息。');
+    });
+});
+
+function bindReceive() {
+    var itemName = 'LeanCloud-realtime-sdk-demo-room-id';
+    var id = localStorage.getItem(itemName);
+    if (!id) {
+        id = conv.id;
+        localStorage.setItem(itemName, id);
+    }
+    conv2 = rt.conv(id);
+    showLog('房间的 id:' + id);
+    conv2.list(function(data) {
+        showLog('列出当前 Conversation 的成员列表：', data);
+    });
+    conv2.receive(function(data) {
+        console.log(data);
+        var text = '';
+        if (data.msg.test) {
+            text = data.msg.test;
+        } else {
+            text = JSON.stringify(data.msg);
+        }
+        showLog('收到新消息：' + text);
+    });
+    window.addEventListener('close', function() {
+        localStorage.removeItem(itemName);
+    });
 }
