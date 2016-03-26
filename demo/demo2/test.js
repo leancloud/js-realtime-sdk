3961a965d0ea72b5229dfafc295a9f2fcee18bfa
leeyeh <lee55962698@gmail.com>
2016-03-26 16:16:03 +0800

    feat(demo): update demo2

--- demo/demo2/test.js
+++ demo/demo2/test.js
@@ -1,5 +1,6 @@
 // 请将 AppId 改为你自己的 AppId，否则无法本地测试
 var appId = '9p6hyhh60av3ukkni3i9z53q1l8yy3cijj6sie3cewft18vm';
+var appKey = 'nhqqc1x7r7r89kp8pggrme57i374h3vyd0ukr2z3ayojpvf4';
 
 // 请换成你自己的一个房间的 conversation id（这是服务器端生成的）
 var roomId = '551a2847e4b04d688d73dc54';
@@ -7,8 +8,8 @@ var roomId = '551a2847e4b04d688d73dc54';
 // 每个客户端自定义的 id
 var clientId = 'LeanCloud';
 
-// 用来存储 realtimeObject
-var rt;
+var client;
+var messageIterator;
 
 // 用来存储创建好的 roomObject
 var room;
@@ -49,124 +50,81 @@ function main() {
     clientId = val;
   }
   if (!firstFlag) {
-    rt.close();
+    client.close();
   }
 
   // 创建实时通信实例
-  rt = AV.realtime({
+  var realtime = new AV.Realtime({
     appId: appId,
-    clientId: clientId,
-
-    // 请注意，这里关闭 secure 完全是为了 Demo 兼容范围更大些
-    // 具体请参考实时通信文档中的「其他兼容问题」部分
-    // 如果真正使用在生产环境，建议不要关闭 secure，具体阅读文档
-    // secure 设置为 true 是开启
-    secure: false
+    appKey: appKey,
   });
-
-  // 监听连接成功事件
-  rt.on('open', function() {
-    firstFlag = false;
+  realtime.createIMClient(clientId)
+  .then(function(c) {
     showLog('服务器连接成功！');
-
-    // 获得已有房间的实例
-    rt.room(roomId, function(object) {
-
-      // 判断服务器端是否存在这个 room，如果存在
-      if (object) {
-        room = object;
-
-        // 当前用户加入这个房间
-        room.join(function() {
-
-          // 获取成员列表
-          room.list(function(data) {
-            showLog('当前 Conversation 的成员列表：', data);
-
-            // 获取在线的 client（Ping 方法每次只能获取 20 个用户在线信息）
-            rt.ping(data.slice(0, 20), function(list) {
-              showLog('当前在线的成员列表：', list);
-            });
-
-            var l = data.length;
-
-            // 如果超过 500 人，就踢掉一个。
-            if (l > 490) {
-              room.remove(data[30], function() {
-                showLog('人数过多，踢掉： ', data[30]);
-              });
-            }
-
-            // 获取聊天历史
-            getLog(function() {
-              printWall.scrollTop = printWall.scrollHeight;
-              showLog('已经加入，可以开始聊天。');
-            });
-          });
-
-        });
-
-        // 房间接受消息
-        room.receive(function(data) {
-          if (!msgTime) {
-            // 存储下最早的一个消息时间戳
-            msgTime = data.timestamp;
-          }
-          showMsg(data);
-        });
-      } else {
-        // 如果服务器端不存在这个 conversation
-        showLog('服务器不存在这个 conversation，你需要创建一个。');
-
-        // 创建一个新 room
-        rt.room({
-          // Room 的默认名字
-          name: 'LeanCloud-Room',
-
-          // 默认成员的 clientId
-          members: [
-            // 当前用户
-            clientId
-          ],
-          // 创建暂态的聊天室（暂态聊天室支持无限人员聊天，但是不支持存储历史）
-          // transient: true,
-          // 默认的数据，可以放 Conversation 名字等
-          attr: {
-            test: 'demo2'
-          }
-        }, function(obj) {
-
-          // 创建成功，后续你可以将 room id 存储起来
-          room = obj;
-          roomId = room.id;
-          showLog('创建一个新 Room 成功，id 是：', roomId);
-
-          // 关闭原连接，重新开启新连接
-          rt.close();
-          main();
-        });
-      }
+    client = c;
+    client.on('disconnect', function() {
+      showLog('服务器正在重连，请耐心等待。。。');
     });
-  });
-
-  // 监听服务情况
-  rt.on('reuse', function() {
-    showLog('服务器正在重连，请耐心等待。。。');
-  });
-
-  // 监听错误
-  rt.on('error', function() {
-    showLog('连接遇到错误。。。');
-  });
+    return c.getConversation(roomId);
+  })
+  .then(function(conversation) {
+    if (conversation) {
+      return conversation;
+    } else {
+      // 如果服务器端不存在这个 conversation
+      showLog('服务器不存在这个 conversation，创建一个。');
+      return client.createConversation({
+        name: 'LeanCloud-Conversation',
+        members: [
+          // 默认包含当前用户
+          'Wallace'
+        ],
+        // 创建暂态的聊天室（暂态聊天室支持无限人员聊天，但是不支持存储历史）
+        // transient: true,
+        // 默认的数据，可以放 conversation 属性等
+        attributes: {
+          test: 'demo2'
+        }
+      }).then(function(conversation) {
+        showLog('创建新 Room 成功，id 是：', roomId);
+        roomId = conversation.id;
+        return conversation;
+      });
+    }
+  })
+  .then(function(conversation) {
+    showLog('当前 Conversation 的成员列表：', conversation.members);
+    if (conversation.length > 490) {
+      conversation.remove(data[30]).then(function() {
+        showLog('人数过多，踢掉： ', data[30]);
+      });
+    }
+    return conversation.join();
+  })
+  .then(function(conversation) {
+    // 获取聊天历史
+    room = conversation;
+    messageIterator = conversation.getMessagesIterator();
+    getLog(function() {
+      printWall.scrollTop = printWall.scrollHeight;
+      showLog('已经加入，可以开始聊天。');
+    });
+    // 房间接受消息
+    conversation.on('message', function(message) {
+      if (!msgTime) {
+        // 存储下最早的一个消息时间戳
+        msgTime = message.timestamp;
+      }
+      showMsg(message);
+    });
+  })
+  .catch(function(err) {
+    console.error(err);
+  })
 }
 
 function sendMsg() {
 
-  // 如果没有连接过服务器
-  if (firstFlag) {
-    alert('请先连接服务器！');
-    return;
-  }
   var val = inputSend.value;
 
   // 不让发送空字符
@@ -175,15 +133,10 @@ function sendMsg() {
   }
 
   // 向这个房间发送消息，这段代码是兼容多终端格式的，包括 iOS、Android、Window Phone
-  room.send({
-    text: val
-  }, {
-    type: 'text'
-  }, function(data) {
-
+  room.send(new AV.TextMessage(val)).then(function(message) {
     // 发送成功之后的回调
     inputSend.value = '';
-    showLog('（' + formatTime(data.t) + '）  自己： ', val);
+    showLog('（' + formatTime(message.timestamp) + '）  自己： ', message.text);
     printWall.scrollTop = printWall.scrollHeight;
   });
 
@@ -210,19 +163,14 @@ function sendMsg() {
 }
 
 // 显示接收到的信息
-function showMsg(data, isBefore) {
-  var text = '';
-  var from = data.fromPeerId;
-  if (data.msg.type) {
-    text = data.msg.text;
-  } else {
-    text = data.msg;
-  }
-  if (data.fromPeerId === clientId) {
+function showMsg(message, isBefore) {
+  var text = message.text;
+  var from = message.from;
+  if (message.from === clientId) {
     from = '自己';
   }
   if (String(text).replace(/^\s+/, '').replace(/\s+$/, '')) {
-    showLog('（' + formatTime(data.timestamp) + '）  ' + encodeHTML(from) + '： ', text, isBefore);
+    showLog('（' + formatTime(message.timestamp) + '）  ' + encodeHTML(from) + '： ', message.text, isBefore);
   }
 }
 
@@ -242,9 +190,7 @@ function getLog(callback) {
     // 标记正在拉取
     logFlag = true;
   }
-  room.log({
-    t: msgTime
-  }, function(data) {
+  messageIterator.next().value.then(function(data) {
     logFlag = false;
     // 存储下最早一条的消息时间戳
     var l = data.length;
@@ -260,6 +206,8 @@ function getLog(callback) {
     if (callback) {
       callback();
     }
+  }).catch(function(err) {
+    console.error(err);
   });
 }
 
