493fa1ec7ed768706f1fb6cdf3e48756bc403c4e
leeyeh <lee55962698@gmail.com>
2016-03-29 15:46:18 +0800

    fix(build): add missing semicolon in travis configs

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 echo "Deploy dist to dist branch.";
-REV=$(git rev-parse HEAD);
-BRANCH=$(git rev-parse --abbrev-ref HEAD);
+REV=`git rev-parse HEAD`;
+BRANCH=`git rev-parse --abbrev-ref HEAD`;
 npm run build;
-git config user.name "leancloud";
-git config user.email "ci@leancloud.rocks";
+if [[ "`git config user.name`" == "" ]]; then
+  git config user.name "leancloud-bot";
+  git config user.email "ci@leancloud.cn";
+fi
 git add dist -f;
 git commit -m "chore(build): build ${REV} [skip ci]";
 git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${BRANCH}:dist;
