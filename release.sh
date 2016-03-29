c1710e25352e6f65355e5d4a2e4fcbfd1b2094d5
leeyeh <lee55962698@gmail.com>
2016-03-30 06:37:53 +0800

    chore(build): keep fixing travis

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 echo "Deploy dist to dist branch.";
 REV=`git rev-parse HEAD`;
 BRANCH=`git rev-parse --abbrev-ref HEAD`;
-npm run build;
-if [[ "`git config user.name`" == "" ]]; then
+test "$(git config user.name)" = '' && (
   git config user.name "leancloud-bot";
   git config user.email "ci@leancloud.cn";
-fi
+)
+npm run build;
 git add dist -f;
 git commit -m "chore(build): build ${REV} [skip ci]";
 git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${BRANCH}:dist;
