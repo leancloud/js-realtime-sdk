ac02fa30fe6a76404ff2496de64e67ae4eb6fcad
leeyeh <lee55962698@gmail.com>
2016-06-26 16:18:36 +0800

    fix: stop assigning global WebSocket
    Previously, globel.WebSocket was assigned accidentally, which caused crashs in React Native as global.WebSocket is readonly.
    
    fix #281

--- src/browser/ws.js
+++ src/browser/ws.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line no-native-reassign
-export default WebSocket = global.WebSocket || global.MozWebSocket;
+const WebSocket = global.WebSocket || global.MozWebSocket;
+export default WebSocket;
