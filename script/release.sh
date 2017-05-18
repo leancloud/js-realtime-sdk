3d6a8c50523fe82e950753950f47d2ab7110d221
leeyeh <lee55962698@gmail.com>
2017-05-18 11:05:24 +0800

    chore(ci): build next branch to next-dist

--- script/release.sh
+++ script/release.sh
@@ -2,14 +2,17 @@
 echo "Deploy dist to dist branch.";
 REV=`git rev-parse HEAD`;
 BRANCH=`git rev-parse --abbrev-ref HEAD`;
+DIST_BRANCH=${1:-dist};
 test "$(git config user.name)" = '' && (
   git config user.name "leancloud-bot";
   git config user.email "ci@leancloud.cn";
 )
-git add dist docs -f;
-git add plugins/typed-messages/dist plugins/typed-messages/docs -f;
-git add plugins/webrtc/dist plugins/webrtc/docs -f;
+git add dist -f;
+git add plugins/typed-messages/dist -f;
+git add plugins/webrtc/dist -f;
+git add plugins/groupchat-receipts/dist -f;
+git add plugins/live-query/dist -f;
 git commit -m "chore(build): build ${REV} [skip ci]";
-git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${BRANCH}:dist;
+git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${BRANCH}:${DIST_BRANCH} > /dev/null 2>&1;
 git reset HEAD~1;
 echo "done.";
