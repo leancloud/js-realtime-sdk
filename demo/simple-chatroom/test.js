7cb7ffa5eb6b57061e9dc10f0f4e315749abd4c8
leeyeh <lee55962698@gmail.com>
2018-04-11 12:05:45 +0800

    refactor(chatroom): reuse Realtime instance

--- demo/simple-chatroom/test.js
+++ demo/simple-chatroom/test.js
@@ -10,7 +10,12 @@ var roomId = '5989569dda2f6000616cfb67';
 // 每个客户端自定义的 id
 var clientId = 'LeanCloud';
 
-var realtime;
+// 创建实时通信实例
+var realtime = new AV.Realtime({
+  appId: appId,
+  appKey: appKey,
+  plugins: AV.TypedMessagesPlugin,
+});
 var client;
 var messageIterator;
 
@@ -52,12 +57,14 @@ bindEvent(document.body, 'keydown', function(e) {
 });
 
 function signUp() {
-  AV.User.signUp(inputName.value, inputPassword.value).then(function(user) {
-    showLog('注册成功');
-    login();
-  }).catch(function(error) {
-    showLog('注册失败：' + error.message);
-  });
+  AV.User.signUp(inputName.value, inputPassword.value)
+    .then(function(user) {
+      showLog('注册成功');
+      login();
+    })
+    .catch(function(error) {
+      showLog('注册失败：' + error.message);
+    });
 }
 
 function login() {
@@ -70,92 +77,99 @@ function login() {
     client.close();
   }
 
-  // 创建实时通信实例
-  realtime = new AV.Realtime({
-    appId: appId,
-    appKey: appKey,
-    plugins: AV.TypedMessagesPlugin,
-  });
   // 创建聊天客户端
-  return AV.User.logIn(clientId, inputPassword.value).then(function(user) {
-    return realtime.createIMClient(user);
-  }).then(function(c) {
-    showLog('连接成功');
-    firstFlag = false;
-    client = c;
-    client.on('disconnect', function() {
-      showLog('[disconnect] 服务器连接已断开');
-    });
-    client.on('offline', function() {
-      showLog('[offline] 离线（网络连接已断开）');
-    });
-    client.on('online', function() {
-      showLog('[online] 已恢复在线');
-    });
-    client.on('schedule', function(attempt, time) {
-      showLog('[schedule] ' + time / 1000 + 's 后进行第 ' + (attempt + 1) + ' 次重连');
-    });
-    client.on('retry', function(attempt) {
-      showLog('[retry] 正在进行第 ' + (attempt + 1) + ' 次重连');
-    });
-    client.on('reconnect', function() {
-      showLog('[reconnect] 重连成功');
-    });
-    client.on('reconnecterror', function() {
-      showLog('[reconnecterror] 重连失败');
-    });
-    // 获取对话
-    return c.getConversation(roomId);
-  })
-  .then(function(conversation) {
-    if (conversation) {
-      return conversation;
-    } else {
-      // 如果服务器端不存在这个 conversation
-      showLog('不存在这个 conversation，创建一个。');
-      return client.createConversation({
-        name: 'LeanCloud-Conversation',
-        // 创建暂态的聊天室（暂态聊天室支持无限人员聊天）
-        transient: true,
-      }).then(function(conversation) {
-        showLog('创建新 Room 成功，id 是：', roomId);
-        roomId = conversation.id;
-        return conversation;
+  return AV.User.logIn(clientId, inputPassword.value)
+    .then(function(user) {
+      return realtime.createIMClient(user);
+    })
+    .then(function(c) {
+      showLog('连接成功');
+      firstFlag = false;
+      client = c;
+      client.on('disconnect', function() {
+        showLog('[disconnect] 服务器连接已断开');
       });
-    }
-  })
-  .then(function(conversation) {
-    return conversation.join();
-  })
-  .then(function(conversation) {
-    // 获取聊天历史
-    room = conversation;
-    messageIterator = conversation.createMessagesIterator();
-    getLog(function() {
-      printWall.scrollTop = printWall.scrollHeight;
-      showLog('已经加入，可以开始聊天。');
-    });
-    // 房间接受消息
-    conversation.on('message', function(message) {
-      if (!msgTime) {
-        // 存储下最早的一个消息时间戳
-        msgTime = message.timestamp;
+      client.on('offline', function() {
+        showLog('[offline] 离线（网络连接已断开）');
+      });
+      client.on('online', function() {
+        showLog('[online] 已恢复在线');
+      });
+      client.on('schedule', function(attempt, time) {
+        showLog(
+          '[schedule] ' +
+            time / 1000 +
+            's 后进行第 ' +
+            (attempt + 1) +
+            ' 次重连'
+        );
+      });
+      client.on('retry', function(attempt) {
+        showLog('[retry] 正在进行第 ' + (attempt + 1) + ' 次重连');
+      });
+      client.on('reconnect', function() {
+        showLog('[reconnect] 重连成功');
+      });
+      client.on('reconnecterror', function() {
+        showLog('[reconnecterror] 重连失败');
+      });
+      // 获取对话
+      return c.getConversation(roomId);
+    })
+    .then(function(conversation) {
+      if (conversation) {
+        return conversation;
+      } else {
+        // 如果服务器端不存在这个 conversation
+        showLog('不存在这个 conversation，创建一个。');
+        return client
+          .createConversation({
+            name: 'LeanCloud-Conversation',
+            // 创建暂态的聊天室（暂态聊天室支持无限人员聊天）
+            transient: true,
+          })
+          .then(function(conversation) {
+            showLog('创建新 Room 成功，id 是：', roomId);
+            roomId = conversation.id;
+            return conversation;
+          });
       }
-      showMsg(message);
+    })
+    .then(function(conversation) {
+      return conversation.join();
+    })
+    .then(function(conversation) {
+      // 获取聊天历史
+      room = conversation;
+      messageIterator = conversation.createMessagesIterator();
+      getLog(function() {
+        printWall.scrollTop = printWall.scrollHeight;
+        showLog('已经加入，可以开始聊天。');
+      });
+      // 房间接受消息
+      conversation.on('message', function(message) {
+        if (!msgTime) {
+          // 存储下最早的一个消息时间戳
+          msgTime = message.timestamp;
+        }
+        showMsg(message);
+      });
+    })
+    .catch(function(err) {
+      console.error(err);
+      showLog('错误：' + err.message);
     });
-  })
-  .catch(function(err) {
-    console.error(err);
-    showLog('错误：' + err.message);
-  })
 }
 
 function sendMsg() {
-
   var val = inputSend.value;
 
   // 不让发送空字符
-  if (!String(val).replace(/^\s+/, '').replace(/\s+$/, '')) {
+  if (
+    !String(val)
+      .replace(/^\s+/, '')
+      .replace(/\s+$/, '')
+  ) {
     alert('请输入点文字！');
   }
 
@@ -163,75 +177,117 @@ function sendMsg() {
   room.send(new AV.TextMessage(val)).then(function(message) {
     // 发送成功之后的回调
     inputSend.value = '';
-    showLog('（' + formatTime(message.timestamp) + '）  自己： ', encodeHTML(message.text));
+    showLog(
+      '（' + formatTime(message.timestamp) + '）  自己： ',
+      encodeHTML(message.text)
+    );
     printWall.scrollTop = printWall.scrollHeight;
   });
-
 }
 
 // 发送多媒体消息示例
 function sendMsgAsFile() {
-
   var val = inputSend.value;
 
   // 不让发送空字符
-  if (!String(val).replace(/^\s+/, '').replace(/\s+$/, '')) {
+  if (
+    !String(val)
+      .replace(/^\s+/, '')
+      .replace(/\s+$/, '')
+  ) {
     alert('请输入点文字！');
   }
   new AV.File('message.txt', {
     base64: b64EncodeUnicode(val),
-  }).save().then(function(file) {
-    return room.send(new AV.FileMessage(file));
-  }).then(function(message) {
-    // 发送成功之后的回调
-    inputSend.value = '';
-    showLog('（' + formatTime(message.timestamp) + '）  自己： ', createLink(message.getFile().url()));
-    printWall.scrollTop = printWall.scrollHeight;
-  }).catch(console.warn);
-
+  })
+    .save()
+    .then(function(file) {
+      return room.send(new AV.FileMessage(file));
+    })
+    .then(function(message) {
+      // 发送成功之后的回调
+      inputSend.value = '';
+      showLog(
+        '（' + formatTime(message.timestamp) + '）  自己： ',
+        createLink(message.getFile().url())
+      );
+      printWall.scrollTop = printWall.scrollHeight;
+    })
+    .catch(console.warn);
 }
 
 function b64EncodeUnicode(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-        return String.fromCharCode('0x' + p1);
-    }));
+  return btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+      return String.fromCharCode('0x' + p1);
+    })
+  );
 }
 
 Usernames = {
   _cache: {},
   get: function(id) {
     if (!this._cache[id]) {
-      this._cache[id] = new AV.Query(AV.User).get(id).then((user) => {
-        var username = user.getUsername();
-        this._cache[id] = username;
-        return username;
-      }).catch(() => {
-        this._cache[id] = id;
-        return id;
-      });
+      this._cache[id] = new AV.Query(AV.User)
+        .get(id)
+        .then(
+          function(user) {
+            var username = user.getUsername();
+            this._cache[id] = username;
+            return username;
+          }.bind(this)
+        )
+        .catch(
+          function() {
+            this._cache[id] = id;
+            return id;
+          }.bind(this)
+        );
     }
     return this._cache[id];
-  }
-}
+  },
+};
 
 // 显示接收到的信息
 function showMsg(message, isBefore) {
   var text = message.text;
-  Promise.resolve().then(function() {
-    if (message.from === clientId) {
-      return '自己';
-    } else {
-      return Usernames.get(message.from);
-    }
-  }).then(function(from) {
-    if (message instanceof AV.TextMessage) {
-      if (String(text).replace(/^\s+/, '').replace(/\s+$/, '')) {
-        showLog('（' + formatTime(message.timestamp) + '）  ' + encodeHTML(from) + '： ', encodeHTML(message.text), isBefore);
+  Promise.resolve()
+    .then(function() {
+      if (message.from === clientId) {
+        return '自己';
+      } else {
+        return Usernames.get(message.from);
       }
-    } else if (message instanceof AV.FileMessage) {
-      showLog('（' + formatTime(message.timestamp) + '）  ' + encodeHTML(from) + '： ', createLink(message.getFile().url()), isBefore);
-    }
-  });
+    })
+    .then(function(from) {
+      if (message instanceof AV.TextMessage) {
+        if (
+          String(text)
+            .replace(/^\s+/, '')
+            .replace(/\s+$/, '')
+        ) {
+          showLog(
+            '（' +
+              formatTime(message.timestamp) +
+              '）  ' +
+              encodeHTML(from) +
+              '： ',
+            encodeHTML(message.text),
+            isBefore
+          );
+        }
+      } else if (message instanceof AV.FileMessage) {
+        showLog(
+          '（' +
+            formatTime(message.timestamp) +
+            '）  ' +
+            encodeHTML(from) +
+            '： ',
+          createLink(message.getFile().url()),
+          isBefore
+        );
+      }
+    });
 }
 
 // 拉取历史
@@ -250,26 +306,29 @@ function getLog(callback) {
     // 标记正在拉取
     logFlag = true;
   }
-  messageIterator.next().then(function(result) {
-    var data = result.value;
-    logFlag = false;
-    // 存储下最早一条的消息时间戳
-    var l = data.length;
-    if (l) {
-      msgTime = data[0].timestamp;
-    }
-    for (var i = l - 1; i >= 0; i--) {
-      showMsg(data[i], true);
-    }
-    if (l) {
-      printWall.scrollTop = printWall.scrollHeight - height;
-    }
-    if (callback) {
-      callback();
-    }
-  }).catch(function(err) {
-    console.error(err);
-  });
+  messageIterator
+    .next()
+    .then(function(result) {
+      var data = result.value;
+      logFlag = false;
+      // 存储下最早一条的消息时间戳
+      var l = data.length;
+      if (l) {
+        msgTime = data[0].timestamp;
+      }
+      for (var i = l - 1; i >= 0; i--) {
+        showMsg(data[i], true);
+      }
+      if (l) {
+        printWall.scrollTop = printWall.scrollHeight - height;
+      }
+      if (callback) {
+        callback();
+      }
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 }
 
 // demo 中输出代码
@@ -292,23 +351,44 @@ function encodeHTML(source) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/\\/g,'&#92;')
-    .replace(/"/g,'&quot;')
-    .replace(/'/g,'&#39;');
+    .replace(/\\/g, '&#92;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
 function formatTime(time) {
   var date = new Date(time);
-  var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+  var month =
+    date.getMonth() + 1 < 10
+      ? '0' + (date.getMonth() + 1)
+      : date.getMonth() + 1;
   var currentDate = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
   var hh = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
   var mm = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
   var ss = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-  return date.getFullYear() + '-' + month + '-' + currentDate + ' ' + hh + ':' + mm + ':' + ss;
+  return (
+    date.getFullYear() +
+    '-' +
+    month +
+    '-' +
+    currentDate +
+    ' ' +
+    hh +
+    ':' +
+    mm +
+    ':' +
+    ss
+  );
 }
 
 function createLink(url) {
-  return '<a target="_blank" href="' + encodeHTML(url) + '">' + encodeHTML(url) + '</a>';
+  return (
+    '<a target="_blank" href="' +
+    encodeHTML(url) +
+    '">' +
+    encodeHTML(url) +
+    '</a>'
+  );
 }
 
 function bindEvent(dom, eventName, fun) {
