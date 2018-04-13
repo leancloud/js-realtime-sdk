263f99ab1b6848d76a92c855db3b008120580fa5
leeyeh <lee55962698@gmail.com>
2018-04-13 15:21:32 +0800

    ci: replace grunt-saucelabs with karma

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,3 +1,5 @@
+export { default as sinon } from 'sinon';
+
 export const listen = (target, resolveEvent, rejectEvent) =>
   new Promise((resolve, reject) => {
     if (resolveEvent) target.once(resolveEvent, (...args) => resolve(args));
@@ -7,10 +9,6 @@ export const listen = (target, resolveEvent, rejectEvent) =>
 export const wait = time => new Promise(resolve => setTimeout(resolve, time));
 
 export const hold = time => result => wait(time).then(() => result);
-
-/* global window */
-export const sinon =
-  (typeof window !== 'undefined' && window.sinon) || require('sinon');
 
 export const series = promiseGens =>
   promiseGens.reduce(
