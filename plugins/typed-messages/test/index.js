3667317e54b0234c330a95ce312288fbf7b5556b
sdjdd <i@sdjdd.com>
2023-03-08 12:28:31 +0800

    fix: test app server

--- plugins/typed-messages/test/index.js
+++ plugins/typed-messages/test/index.js
@@ -1,5 +1,5 @@
 import AV from 'leancloud-storage';
-import { APP_ID, APP_KEY } from '../../../test/configs';
+import { APP_ID, APP_KEY, SERVER } from '../../../test/configs';
 
 import './file-message-and-subclasses';
 import './location-message';
@@ -7,4 +7,5 @@ import './location-message';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY,
+  serverURLs: SERVER,
 });
